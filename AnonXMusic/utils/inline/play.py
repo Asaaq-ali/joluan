848d6e6cc23f4271ddded1ca771217dc34956b0b
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-06 04:06:27 +0300

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -35,38 +35,38 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "◉—————————"
+        bar = "©—————————"
     elif 10 < umm < 20:
-        bar = "—◉————————"
+        bar = "—©————————"
     elif 20 <= umm < 30:
-        bar = "——◉———————"
+        bar = "——©———————"
     elif 30 <= umm < 40:
-        bar = "———◉——————"
+        bar = "———©——————"
     elif 40 <= umm < 50:
-        bar = "————◉—————"
+        bar = "————©—————"
     elif 50 <= umm < 60:
-        bar = "—————◉————"
+        bar = "—————©————"
     elif 60 <= umm < 70:
-        bar = "——————◉———"
+        bar = "——————©———"
     elif 70 <= umm < 80:
-        bar = "———————◉——"
+        bar = "———————©——"
     elif 80 <= umm < 95:
-        bar = "————————◉—"
+        bar = "————————©—"
     else:
-        bar = "—————————◉"
+        bar = "—————————©"
     buttons = [
+        [
+            InlineKeyboardButton(
+                text=f"{played} {bar} {dur}",
+                callback_data="GetTimer",
+            )
+        ],
         [
             InlineKeyboardButton(text="▶️", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="⏸", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="🔄", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="⏭", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="⏹", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        [
-            InlineKeyboardButton(
-                text=f"{played} {bar} {dur}",
-                callback_data="GetTimer",
-            )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
         [
