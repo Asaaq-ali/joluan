11d2e5c92a8203c48624b3991eeaaad62996cdaf
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-11 20:49:49 +0300

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -35,25 +35,25 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "©—————————"
+        bar = "▁▇▄▂★▄▅▃▄"
     elif 10 < umm < 20:
-        bar = "—©————————"
+        bar = "▁▃▄▂▇★▃▅▃"
     elif 20 <= umm < 30:
-        bar = "——©———————"
+        bar = "▃▁▇▂▃▄★▃▅"
     elif 30 <= umm < 40:
-        bar = "———©——————"
+        bar = "▁▄★▂▇▄▄▅▃"
     elif 40 <= umm < 50:
-        bar = "————©—————"
+        bar = "▃▁▇▂▃▄★▃▅"
     elif 50 <= umm < 60:
-        bar = "—————©————"
+        bar = "▁▄★▂▇▄▄▅▃"
     elif 60 <= umm < 70:
-        bar = "——————©———"
+        bar = "▁▃▄▂▇★▃▅▃"
     elif 70 <= umm < 80:
-        bar = "———————©——"
+        bar = "▁▄★▂▇▄▄▅▃"
     elif 80 <= umm < 95:
-        bar = "————————©—"
+        bar = "▁▇▄▂★▄▅▃▄"
     else:
-        bar = "—————————©"
+        bar = "▁▃▄▂▇★▃▅▃"
     buttons = [
         [
             InlineKeyboardButton(
@@ -64,7 +64,6 @@ def stream_markup_timer(_, chat_id, played, dur):
         [
             InlineKeyboardButton(text="▶️", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="⏸", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="🔄", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="⏭", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="⏹", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
